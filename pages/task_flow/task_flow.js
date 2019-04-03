ab596b03776081c8caa217f13bbcf01c4b42939f
Leochens <18332518328@163.com>
2019-04-03 10:17:22 +0800

    task_flow列表按照日期时间排序

--- pages/task_flow/task_flow.js
+++ pages/task_flow/task_flow.js
@@ -3,7 +3,7 @@ import {
   connect
 } from '../../libs/wechat-weapp-redux';
 const app = getApp();
-
+import { compareDate } from '../../utils/util';
 const page = {
 
   /**
@@ -36,45 +36,74 @@ const page = {
     const { id, tf_name, tf_describe, leader_id, is_completed, tasks, members, begin_time, end_time,
       category } = _tf;
 
+    const classfiedTasks = this.classifyTask(tasks);
     this.setData({
-      id,tf_describe,tf_name,is_completed,tasks,begin_time,end_time,category,members,
-      leader: members.filter(mem=>mem.id === leader_id)[0]
+      id, tf_describe, tf_name, is_completed, begin_time, end_time, category, members,
+      leader: members.filter(mem => mem.id === leader_id)[0],
+      tasks:classfiedTasks
     })
+
+
+  },
+  classifyTask: function (t) {
+    // 将子任务按照开始日期分类 同一天的放进一个数组里 还要排序 日期大类排序决定渲染顺序  
+    if (!Array.isArray(t)) return;
+    const tasks = t.slice();
+    // 2019-3-20 12:34:21
+    // const dateTimes = tasks.map(task=>task.begin_time);
+    const sortby = (t1, t2) => {
+      return compareDate(t1.begin_time, t2.begin_time);
+    }
+    tasks.sort(sortby);
+    const splitDate = d => d.split(' ')[0];
+    // 日期相同的抽出来放在一个集合里
+    for (let i = 0; i < tasks.length; i++) {
+      // 如果和上一个的日期一样 就不渲染dot 不一样就渲染dot
+      if (i === 0) {
+        tasks[i].renderDot = true;
+        continue;
+      };
+      if (splitDate(tasks[i].begin_time) === splitDate(tasks[i - 1].begin_time)) {
+        tasks[i].renderDot = false;
+      } else tasks[i].renderDot = true;
+    }
+    console.log(tasks.map(task => task.renderDot));
+    return tasks;
   },
   // 加新的子任务
-  addTask:function(){
-    const {begin_time,end_time} = this.data;
+  addTask: function () {
+    const { begin_time, end_time } = this.data;
     const time = {
-      beginDate:begin_time,
-      endDate:end_time
+      beginDate: begin_time,
+      endDate: end_time
     }
     wx.navigateTo({
-      url: './create_task/create_task?time='+JSON.stringify(time)+"&tf_id="+this.data.id+"&members="+JSON.stringify(this.data.members),
+      url: './create_task/create_task?time=' + JSON.stringify(time) + "&tf_id=" + this.data.id + "&members=" + JSON.stringify(this.data.members),
     })
   },
   // 邀请新成员
-  addMember:function(){
+  addMember: function () {
     wx.navigateTo({
       url: './add_member/add_member',
     })
   },
   // 加入星标
-  addFavor: function(){
+  addFavor: function () {
     console.log("加入星标");
   },
   // 任务数据
-  checkData: function(){
+  checkData: function () {
     wx.navigateTo({
       url: './task_flow_data/task_flow_data',
     })
   },
-  my_tasks:function(){
+  my_tasks: function () {
     wx.navigateTo({
       url: './my_tasks/my_tasks',
     })
   },
   // 查看全部成员
-  allMembers: function(){
+  allMembers: function () {
 
   },
 
