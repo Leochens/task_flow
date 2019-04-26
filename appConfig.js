fdbe6b857f6d5aaa3cc319dcea616bca4fc18bbd
Leochens <18332518328@163.com>
2019-04-26 15:31:26 +0800

    修改session有效期

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,8 @@
 const appConfig = {
-  apiBaseUrl: 'http://localhost:8899',
+  // apiBaseUrl: 'http://localhost:8899',
   // apiBaseUrl: 'http://192.168.1.101:8899',
-  // apiBaseUrl: 'https://api.mokis.top',
-  expiration: 1000 * 60 * 60, //设置SID过期时间 1个小时
+  apiBaseUrl: 'https://api.mokis.top',
+  expiration: 1000 * 60 * 60 * 24 * 7, //设置SID过期时间 1个小时
 };
 // const env = 'prod'
 const env = 'dev'
