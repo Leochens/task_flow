30282910689ecd69b2050578cf7239dcf8b608b7
Leochens <18332518328@163.com>
2019-05-10 14:42:24 +0800

    UI美化 刘海大波浪

--- common/nav_bar/nav_bar.js
+++ common/nav_bar/nav_bar.js
@@ -1,40 +1,54 @@
-// common/nav_bar/nav_bar.js
 const app = getApp();
 Component({
   /**
-   * 组件的属性列表
+   * 组件的一些选项
+   */
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true
+  },
+  /**
+   * 组件的对外属性
    */
   properties: {
-    title: {
-      type: String
+    bgColor: {
+      type: String,
+      default: ''
     },
-    onBack: {
-      type: Function,
-      value: () => { wx.navigateBack() }
+    isCustom: {
+      type: [Boolean, String],
+      default: false
     },
-    blank: {
-      type: Boolean
+    isBack: {
+      type: [Boolean, String],
+      default: false
     },
-    operation: {
-      type: Boolean
-    }
+    bgImage: {
+      type: String,
+      default: ''
+    },
   },
-
   /**
    * 组件的初始数据
    */
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    Custom: app.globalData.Custom,
+    Custom: app.globalData.Custom
   },
-
   /**
    * 组件的方法列表
    */
   methods: {
-    back: function () {
-      this.properties.onBack();
+    BackPage() {
+      wx.navigateBack({
+        delta: 1
+      });
+    },
+    toHome() {
+      wx.reLaunch({
+        url: '/pages/index/index',
+      })
     }
   }
 })
